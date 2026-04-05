e1592371dd6f3d60b37637b98f345f8ec2f3f616
Nazife Atmaca <nazifeatmaca622@gmail.com>
2026-04-05 20:14:19 +0300

    FutureBuilder

--- part25_http/lib/local_json.dart
+++ part25_http/lib/local_json.dart
@@ -16,22 +16,57 @@ class _LocalJsonState extends State<LocalJson> {
   Widget build(BuildContext context) {
     carJsonOku();
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Local Json Kullanımı"),
+      appBar: AppBar(title: Text("Local Json Kullanımı")),
+      body: FutureBuilder(
+        future: carJsonOku(),
+        builder: (context, snapchot) {
+          if (snapchot.hasData) {
+            List<Car> arabalar =
+                snapchot.data!; //datada futuredan aldığımız veriler vardır
+            return ListView.builder(
+              itemCount: arabalar.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(arabalar[index].carName),
+                  subtitle: Text(arabalar[index].country),
+                  leading: Container(
+                    color: Colors.grey,
+                    child: Text(arabalar[index].model[0].price.toString()),
+                  ),
+                );
+              },
+            );
+          } else if (snapchot.hasError) {
+            return Center(child: Text(snapchot.error.toString()));
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
       ),
-      body: Center(),
     );
   }
 
-  Future<void> carJsonOku() async{
-    String okunanString= await DefaultAssetBundle.of(context).loadString("assets/data/cars.json");
-    var jsonObject=jsonDecode(okunanString);
+  Future<List<Car>> carJsonOku() async {
+    try {
+      Future.delayed(Duration(seconds: 10));
+      String okunanString = await DefaultAssetBundle.of(
+        context,
+      ).loadString("assets/data/cars.json");
+      var jsonObject = jsonDecode(okunanString);
 
-    //jsonObjecten gelen map yapısı car sınıfında tanımladığımı constructer fromJson ile Car nesnesine yerleştirirlir.
-    //map ile her bir araba nesnesi sırayla yerleştirilir.
-    List<Car> allCar=(jsonObject as List).map((arabaMap)=>Car.fromJson(arabaMap)).toList();
-    debugPrint(allCar[1].model[1].modelName.toString());
-    
+      //jsonObjecten gelen map yapısı car sınıfında tanımladığımı constructer fromJson ile Car nesnesine yerleştirirlir.
+      //map ile her bir araba nesnesi sırayla yerleştirilir.
+      List<Car> allCar = (jsonObject as List)
+          .map((arabaMap) => Car.fromJson(arabaMap))
+          .toList();
+      debugPrint(allCar[1].model[1].modelName.toString());
+
+      return allCar;
+    } catch (e) {
+      print("Hata: $e");
+      return Future.error(e.toString());
+    }
+
     /* debugPrint(okunanString);
     print("*****************************");
     List arabaListesi=jsonObject;
