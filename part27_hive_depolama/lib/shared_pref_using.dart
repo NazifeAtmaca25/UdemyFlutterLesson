2c3ff0dd2895e8a8b65efd21aec9a66d86953599
Nazife Atmaca <nazifeatmaca622@gmail.com>
2026-05-26 23:40:13 +0300

    sharedPreferences ile verileri okuyup yazmak

--- part27_hive_depolama/lib/shared_pref_using.dart
+++ part27_hive_depolama/lib/shared_pref_using.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:part27_hive_depolama/model/my_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPrefUsing extends StatefulWidget {
   const SharedPrefUsing({super.key});
@@ -11,9 +12,15 @@ class SharedPrefUsing extends StatefulWidget {
 
 class _SharedPrefUsingState extends State<SharedPrefUsing> {
   var _secilenCinsiyet = Cinsiyet.KADIN;
-  var _secilenRenkler = [];
+  var _secilenRenkler = <String>[];
   var _ogrenciMi = false;
   final TextEditingController _nameController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    verileriGetir();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,61 +34,75 @@ class _SharedPrefUsingState extends State<SharedPrefUsing> {
               decoration: InputDecoration(labelText: "Adınızı giriniz"),
             ),
           ),
-          
+
           RadioGroup(
             groupValue: _secilenCinsiyet,
-              onChanged: (Cinsiyet? secilmisCinsiyet){
-            setState(() {
-              _secilenCinsiyet=secilmisCinsiyet!;
-            });
-          }, child: Column(
-            children: [
-              buildRadioListTile("Kadın", Cinsiyet.KADIN),
-              buildRadioListTile("Erkek", Cinsiyet.ERKEK),
-              buildRadioListTile("Diğer", Cinsiyet.DIGER),
-            ],
-          )),
+            onChanged: (Cinsiyet? secilmisCinsiyet) {
+              setState(() {
+                _secilenCinsiyet = secilmisCinsiyet!;
+              });
+            },
+            child: Column(
+              children: [
+                for (var item in Cinsiyet.values)
+                  buildRadioListTile(item.name, item),
+              ],
+            ),
+          ),
 
-          buildCheckboxListTile(Renkler.KIRMIZI),
-          buildCheckboxListTile(Renkler.MAVI),
-          buildCheckboxListTile(Renkler.MOR),
-          buildCheckboxListTile(Renkler.PEMBE),
-          buildCheckboxListTile(Renkler.SARI),
-          
+          for (var item in Renkler.values) buildCheckboxListTile(item),
+
           SwitchListTile(
             title: Text("Öğrenci misin"),
-              value: _ogrenciMi, onChanged: (bool ogrenciMi){
-            setState(() {
-              _ogrenciMi=ogrenciMi;
-            });
-          }),
-          TextButton(onPressed: (){}, child: Text("Kaydet"))
+            value: _ogrenciMi,
+            onChanged: (bool ogrenciMi) {
+              setState(() {
+                _ogrenciMi = ogrenciMi;
+              });
+            },
+          ),
+          TextButton(onPressed: _verileriKaydet, child: Text("Kaydet")),
         ],
       ),
     );
   }
+  void _verileriKaydet() async{
+    final name=_nameController.text;
+    final prefences= await SharedPreferences.getInstance();
+    
+    prefences.setString("isim", name);
+    prefences.setBool("ogrenci", _ogrenciMi);
+    prefences.setInt("cinsiyet", _secilenCinsiyet.index);
+    prefences.setStringList("renkler", _secilenRenkler);
+    debugPrint("cinsiyet:${_secilenCinsiyet.index} renkler:$_secilenRenkler öğrenci:$_ogrenciMi");
+  }
+
+  void verileriGetir() async{
+    final preferences=await SharedPreferences.getInstance();
+    _nameController.text=preferences.getString("isim") ?? "";
+    _ogrenciMi=preferences.getBool("ogrenci") ?? false;
+    _secilenCinsiyet=Cinsiyet.values[preferences.getInt("cinsiyet") ?? 0];
+    _secilenRenkler=preferences.getStringList("renkler") ?? <String>[];
+  }
 
   CheckboxListTile buildCheckboxListTile(Renkler renk) {
     return CheckboxListTile(
-          title: Text(renk.name),
-          value: _secilenRenkler.contains(renk.name),
-          onChanged: (bool? deger){
-            if(deger== false){
-              _secilenRenkler.remove(renk.name);
-            } else{
-              _secilenRenkler.add(renk.name);
-            }
-            setState(() {
-              debugPrint(_secilenRenkler.toString());
-            });
-          },
-        );
+      title: Text(renk.name),
+      value: _secilenRenkler.contains(renk.name),
+      onChanged: (bool? deger) {
+        if (deger == false) {
+          _secilenRenkler.remove(renk.name);
+        } else {
+          _secilenRenkler.add(renk.name);
+        }
+        setState(() {
+          debugPrint(_secilenRenkler.toString());
+        });
+      },
+    );
   }
 
   RadioListTile<Cinsiyet> buildRadioListTile(String title, Cinsiyet cinsiyet) {
-    return RadioListTile<Cinsiyet>(
-      value: cinsiyet,
-      title: Text(title),
-    );
+    return RadioListTile<Cinsiyet>(value: cinsiyet, title: Text(title));
   }
 }
